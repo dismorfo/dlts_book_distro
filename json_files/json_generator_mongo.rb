62cfdf518fec33491fb1672ec071498b1f36c336
Kate Pechekhonova <katepechekhonova@DLTS-2FL-MBP01.home>
2015-05-28 23:17:30 -0400

    Added mongo json and againg fixed json files

--- json_files/json_generator_mongo.rb
+++ json_files/json_generator_mongo.rb
@@ -1,15 +1,20 @@
 require 'rubygems'
 require 'json'
+require 'mongo'
 
-file = File.open("columbia_aco000020.txt", "r")
+include Mongo
+
+file = File.open("columbia_aco000020_2up.txt", "r")
 #file = File.open("sample.txt", "r")
+client=Mongo::Client.new('mongodb://127.0.0.1:27017/books')
 
 # get header
 keys = file.readline().chomp!.split(" ")
 values = []
-result = {} 
 agreg = []
 assoc={}
+assoc_member={}
+result={}
 # parse values
 file.each_line.with_index do | line,index |
   assoc_member = {}
@@ -19,16 +24,18 @@ file.each_line.with_index do | line,index |
     if iter>0 
       assoc_member[keys[iter]] = v
     else
-      filename= v[0..-5]
+      filename= v[0..-7]
     end
   end
   filename_jpg = "public://"+filename+"_s"+".jpg"
   f_schema= "public://"+filename+"_d.jp2" 
   assoc_member[keys[0]]= f_schema
-  assoc["cm"]=assoc_member
-  assoc["ri"]= filename_jpg
-  assoc["id"] = filename
+  assoc[:cm]=assoc_member
+  assoc[:ri]= filename_jpg
+  assoc[:_id] = filename
+  result1 = client[:"books_json"].insert_one(assoc)
   agreg[index] = assoc
 end
-result["columbia_aco000020"]=agreg
-puts result.to_json
+#result["book"]=agreg
+#result1 = client[:"books_json_rev"].insert_one(agreg)
+puts result1
